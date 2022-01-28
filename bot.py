02f17b6e129c2dc45d326bc16ec26b18eda422ff
drawizmonarch12 <97831430+drawizmonarch12@users.noreply.github.com>
2022-01-28 09:12:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import fnmatch
 import asyncio
 from telebot.async_telebot import AsyncTeleBot
 
-botToken = 5286223389:AAHI_d6AmnuDsNbOomWYIQJqEEudi1NRBNA
+botToken = "5286223389:AAHI_d6AmnuDsNbOomWYIQJqEEudi1NRBNA"
 
 bot = AsyncTeleBot(botToken, parse_mode=None)
 
